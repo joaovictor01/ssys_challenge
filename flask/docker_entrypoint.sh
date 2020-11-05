fa3b3362ed9bc77cb8f6b97f19913663a92cdf43
João Victor <joao_victor-321@hotmail.com>
2020-11-05 01:06:02 -0300

    Update

--- flask/docker_entrypoint.sh
+++ flask/docker_entrypoint.sh
@@ -1,10 +1,4 @@
 #!/usr/bin/env bash
 export FLASK_APP=main.py
-# export FLASK_ENV=development
-# flask db init
-# flask db migrate
-# flask db upgrade
-
-# export FLASK_APP=main.py
 export FLASK_ENV=production
 uwsgi app.ini
